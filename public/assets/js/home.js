a4bbfbecd8bc69a3a4d5e2fbce29b2f760347391
Ness <johnsondavid1107@gmail.com>
2021-01-04 21:23:06 -0500

    minor updates

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -8,15 +8,14 @@ $(document).ready(function () {
     $.ajax("/api/entries", {
         type: "GET"
     }).then(function (response) {
-        console.log(response)
-        //need to change [0] for the current user logged in's data
-        for (let i = 0; i < response[0].Entries.length; i++) {
-            console.log(response)
-            let dataInfo = response[0].Entries[i].person
+
+
+        for (let i = 0; i < response.Entries.length; i++) {
+            let dataInfo = response.Entries[i].person
             let newLiItem = $("<li>")
             newLiItem.addClass("list-group-item")
             newLiItem.html(dataInfo)
-            newLiItem.attr("data", response[0].Entries[i].id)
+            newLiItem.attr("data", response.Entries[i].id)
             $(".list-group").append(newLiItem)
 
         }
